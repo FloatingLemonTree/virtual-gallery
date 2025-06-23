e150135be46390972e13372e39f74ae085b641e4
Lemon Tree <serushev16@gmail.com>
2025-06-23 22:44:59 +0500

    Test Commit

--- src/components/ThreeScene.tsx
+++ src/components/ThreeScene.tsx
@@ -1,7 +1,7 @@
 'use client'; // если ты используешь App Router
 
-import { useEffect, useRef } from "react";
-import * as THREE from "three";
+import { useEffect, useRef } from 'react';
+import * as THREE from 'three';
 
 const Scene = () => {
   const mountRef = useRef<HTMLDivElement | null>(null);
